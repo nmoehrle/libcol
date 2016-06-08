b2afdddf71645878f4c3f04504cd95dab577427a
Nils Moehrle <nils@kuenstle-moehrle.de>
2016-06-08 20:13:03 +0300

    Initial import

---
+++ defines.h
@@ -0,0 +1,9 @@
+#ifndef COL_DEFINES_HEADER
+#define COL_DEFINES_HEADER
+
+#define COL_NAMESPACE_BEGIN namespace col {
+#define COL_NAMESPACE_END }
+#define MAPS_NAMESPACE_BEGIN namespace maps {
+#define MAPS_NAMESPACE_END }
+
+#endif /* COL_DEFINES_HEADER */
