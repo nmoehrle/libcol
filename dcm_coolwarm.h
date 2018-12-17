64b67da964255a184219bda1430f5b8886356481
Nils Moehrle <nils@kuenstle-moehrle.de>
2018-12-17 22:21:35 +0100

    Add license

--- dcm_coolwarm.h
+++ dcm_coolwarm.h
@@ -1,4 +1,12 @@
 /*
+ * Copyright (C) 2016-2018, Nils Moehrle
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD 3-Clause license. See the LICENSE.txt file for details.
+ */
+
+/*
  * This colormap was created using a own implementation of the divergent cool
  * warm colormap described in:
  *
--- mpl_plasma.h
+++ mpl_plasma.h
@@ -1,4 +1,10 @@
 /*
+ * Copyright (C) 2016-2018, Nils Moehrle
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD 3-Clause license. See the LICENSE.txt file for details.
+ *
  * Derived from https://github.com/BIDS/colormap/blob/master/colormaps.py
  *
  * Original copyright notice:
